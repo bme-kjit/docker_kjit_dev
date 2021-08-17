1df8f194da512343eac1c21782f6127b6ce98486
Szőke László <szoke.laszlo95@gmail.com>
2021-08-17 08:26:45 +0200

    working ros container

--- ros/entry.sh
+++ ros/entry.sh
@@ -2,6 +2,6 @@
 echo "$HOST_USER:x:$HOST_UID:" >> /etc/group
 echo "docker:x:999:$HOST_USER" >> /etc/group
 
-su -c "source /opt/ros/melodic/setup.bash && rosdep update && source $CATKIN_WS/devel/setup.bash" $HOST_USER
+su -c "echo 'source /opt/ros/noetic/setup.bash && rosdep update && source $CATKIN_WS/devel/setup.bash' >> /home/$HOST_USER/.bashrc" $HOST_USER
 su $HOST_USER
-
+source /opt/ros/noetic/setup.bash && rosdep update && source $CATKIN_WS/devel/setup.bash
