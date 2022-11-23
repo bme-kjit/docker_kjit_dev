8ea4a0a66352b66f334e589c093c43e1ce7f938b
Szoke Laszlo <szoke.laszlo95@gmail.com>
2022-11-23 12:27:38 +0100

    added starter script

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /bin/bash
 if [ -z "$1" ]; then
     START_SSH=false
 else
@@ -6,15 +6,8 @@ else
     shift
 fi
 
-echo "$HOST_USER:x:$HOST_UID:" >> /etc/group
-echo "docker:x:999:$HOST_USER" >> /etc/group
-useradd -u $HOST_UID -g $HOST_UID -d /home/$HOST_USER -s /bin/bash -M $HOST_USER
-
-export DISPLAY=`echo $DISPLAY | sed 's/^[^:]*\(.*\)/172.17.0.1\1/'`
-export PYTHONPATH=$PYTHONPATH:/home/$HOST_USER/workspace:/home/$HOST_USER/remote_deployment
-export SUMO_HOME=/usr/share/sumo
-
 if [ "$START_SSH" = true ]; then
     service ssh start
 fi
-su $HOST_USER "$@"
+echo "Please leave this terminal open, go to a new terminal window and run ./start_docker.sh"
+su "$@"
